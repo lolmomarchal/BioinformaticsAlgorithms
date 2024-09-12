2263a1413681705565f55b9732aad8eaedd6824c
Lorenzo Olmo Marchal <114376800+lolmomarchal@users.noreply.github.com>
2024-09-11 21:09:18 -0700

    Update chp1.java

--- BOOK/java/chp1.java
+++ BOOK/java/chp1.java
@@ -90,6 +90,49 @@ public static List<Integer> PatternMatching(String Pattern, String Genome) {
     return index;
 
 }
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
+// Please do not remove package imports because these are used by the autograder.
+
+public class Main { // Please do not change this class declaration as this line is used by the autograder.
+public static Map<String, Integer> FrequencyTable(String Text, int k){
+    Map<String, Integer> freq_map = new HashMap<>();
+    for(int i = 0; i < Text.length()-k+1; i++){
+        String substring_kmer = Text.substring(i, i+k);
+        if (freq_map.containsKey(substring_kmer)){
+            freq_map.put(substring_kmer, freq_map.get(substring_kmer)+1);
+        }
+        else{
+            freq_map.put(substring_kmer, 1);
+        }
+    }
+    return freq_map;
+}
+
+// Insert your FrequentWords function here, along with any subroutines you need
+
+// Insert your FindClumps function here, along with any subroutines you need
+public static List<String> FindClumps(String Genome, int k, int L, int t) {
+    List<String> Patterns = new ArrayList<>();
+    for( int i = 0; i < Genome.length()-L+1;i++){
+        String window = Genome.substring(i, i+L);
+        Map<String, Integer> freqMap = FrequencyTable(window, k);
+        for(String key: freqMap.keySet()){
+            if(freqMap.get(key) >= t && Patterns.contains(key) == false){
+                Patterns.add(key);
+            }
+        }
+        
+    }
+    return Patterns;
+
+}
+
+//Please do not provide a closing bracket for the Main class, as the autograder will handle this.
 //Please do not provide a closing bracket for the Main class, as the autograder will handle this.
 //Please do not provide a closing bracket for the Main class, as the autograder will handle this.
